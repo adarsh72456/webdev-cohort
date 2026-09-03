3b669298c00b2a783dfb89e8a6a1ae6117d16ccc
Adarsh Gautam <87937051+adarsh72456@users.noreply.github.com>
2026-09-03 22:03:19 +0530

    Feat/multiply (#1)
    * add multiply function to perform multiplication
    
    * add function call to multiply for testing
    
    * add additional test case for multiply function
    
    * add additional test case for multiply function
    
    * add missing test case for multiply function

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function subtract(a, b) {
     if (!(a > b)) throw new Error("a must be greater than b");
     return a - b;
 }
+function multiply(a, b) {
+    return a * b;
+}
+
 add(5, 3);
 add(10, 23);
 add(40, 42);
@@ -13,6 +17,10 @@ add(100, 200);
 
 subtract(10, 3);
 
+multiply(5, 3);
+multiply(10, 23);
+multiply(40, 42);
+multiply(100, 200);
 
 
 
