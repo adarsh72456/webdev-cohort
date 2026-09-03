0f215a8e020f0af6a375d9cc0b91e0d79d305ee4
Adarsh Gautam <adarshgautam331@gmail.com>
2026-09-03 21:49:03 +0530

    add multiply function to perform multiplication

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function subtract(a, b) {
     if (!(a > b)) throw new Error("a must be greater than b");
     return a - b;
 }
+function multiply(a, b) {
+    return a * b;
+}
+
 add(5, 3);
 add(10, 23);
 add(40, 42);
